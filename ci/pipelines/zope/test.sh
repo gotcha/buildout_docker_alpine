52a6749e18ff7b9a9b1c8a0517cd18ce4b2c9c69
Godefroid Chapelle <gotcha@bubblenet.be>
2019-08-07 15:28:53 +0200

    fix fully absolute

--- ci/pipelines/zope/test.sh
+++ ci/pipelines/zope/test.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # TODO: kill zope before exit
 MAX=${1:-30}
-nohup /zope/bin/instance fg &
+nohup "$HOME/bin/instance fg" &
 c=0
-while ! grep -s -q 'Zope Ready to handle requests' nohup.out; do
+while ! grep -s -q 'Zope Ready to handle requests' "$HOME/nohup.out"; do
 	sleep 1
 	echo "$c seconds"
 	c=$((c + 1))
 	if [[ c -eq $MAX ]]; then
-		echo "Could not start: $(cat nohup.out)"
+		echo "Could not start: $(cat "$HOME/nohup.out")"
 		exit 1
 	fi
 done
