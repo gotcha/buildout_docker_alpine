46496ada90074aa2ac6f8095ccb2d0037b43ae84
Jean-François Roche <jfroche@affinitic.be>
2019-08-07 13:27:05 +0200

    feat: add script to test zope startup

---
+++ ci/pipelines/zope/test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# TODO: kill zope before exit
+MAX=${1:-30}
+nohup bin/instance fg &
+c=0
+while ! grep -s -q 'Zope Ready to handle requests' nohup.out; do
+	sleep 1
+	echo "$c seconds"
+	c=$((c + 1))
+	if [[ c -eq $MAX ]]; then
+		echo "Could not start: $(cat nohup.out)"
+		exit 1
+	fi
+done
+echo "OK"
